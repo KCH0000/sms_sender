5f58ed1ae568dfe0477eb339ffcb120959ac21bd
Kirill Sidorov <kirillsidorov@gmail.com>
2019-08-12 18:16:23 +0300

    Blacklist

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from sender import load_data_from_csv, send_sms
 from log.logger import response_logger
+from time import sleep
 
 MESSAGE = '17 августа 13.00 состоится ' \
           'собрание собственников, членов СНТ "Астра"'
@@ -7,9 +8,11 @@ MESSAGE = '17 августа 13.00 состоится ' \
 source_files = [
     {'name': 'astra_phones_1.csv', 'encoding': 'UTF-8'},
     {'name': 'astra_phones_2.csv', 'encoding': 'UTF-8'},
-    {'name': 'astra_phones_4.csv', 'encoding': 'UTF-8'}
+    {'name': 'astra_phones_4.csv', 'encoding': 'UTF-8'},
+    {'name': 'astra_phones_5.csv', 'encoding': 'UTF-8'}
 ]
 
+blacklist_file = 'blacklist.csv'
 
 sender_list = []
 
@@ -17,7 +20,10 @@ for file in source_files:
     sender_list += load_data_from_csv(file['name'], file['encoding'])
     response_logger.info(f'Загружены данные из {file["name"]}, всего {len(sender_list)} номеров')
 
-sender_list = set(sender_list)
+blacklist = load_data_from_csv(blacklist_file)
+response_logger.info(f'Вчерноем списке {len(blacklist)} номеров')
+
+sender_list = set(sender_list) - set(blacklist)
 response_logger.info(f'Всего уникальных {len(sender_list)} номеров')
 
 if len(MESSAGE) > 70:
@@ -26,4 +32,7 @@ if len(MESSAGE) > 70:
 
 response_logger.info(f'Длина сообщения {len(MESSAGE)} символов')
 
-send_sms('79266023332', MESSAGE)
+# for sender in sender_list:
+#     sleep(1)
+#     send_sms(sender, MESSAGE)
+# send_sms('79266023332', MESSAGE, 'SMSC.RU')
